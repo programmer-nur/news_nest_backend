d6cdb00a7697c6e95879493afaf39188411581ac
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-27 21:18:30 +0600

    added loginusr controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -4,6 +4,7 @@ import tryAsync from '../../../shared/tryAsync';
 import { UserService } from './user.service';
 import sendResponse from '../../../shared/sendResponse';
 import { IUser } from './user.interface';
+import config from '../../../config';
 
 const createUser = tryAsync(async (req: Request, res: Response) => {
   const { ...userData } = req.body;
@@ -16,4 +17,22 @@ const createUser = tryAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const UserController = { createUser };
+const loginUser = tryAsync(async (req: Request, res: Response) => {
+  const { ...loginData } = req.body;
+  const result = await UserService.loginUser(loginData);
+
+  const { refreshToken, ...others } = result;
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Refresh token create successfully!',
+    data: others,
+  });
+});
+export const UserController = { createUser, loginUser };
