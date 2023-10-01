b24e75ed2f6e0e7ee6fe19e4be5f3e304655402b
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-01 09:17:03 +0600

    feat: create news all data  get route

--- src/app/modules/news/news.route.ts
+++ src/app/modules/news/news.route.ts
@@ -4,5 +4,6 @@ import { NewsController } from './news.controller';
 const router = express.Router();
 
 router.post('/', NewsController.insertIntoDb);
+router.post('/', NewsController.getAllFromDb);
 
 export const NewsRoutes = router;
